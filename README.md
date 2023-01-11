44b43163e1d311de2ed2cc6d4d821056b460e734
J <59581793+daechi73@users.noreply.github.com>
2023-01-10 23:56:41 -0500

    added -index.js: completed play() function

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Odin-RPS
+
 What is needed:
 
 -User input(UI) : rock/paper/scissors;
 -computer input(CI): randomlly generated rock/paper/scissors;
 -conditional statements:
-	if UI==rock && CI==Scissors: User wins;
-	if UI==rock && CI==paper: Computer Wins;
-	if UI==rock && CI==rock: tie;
-	if UI==Scissors && CI==Scissors: tie;
-	if UI==Scissors && Ci==rock: Computer wins;
-	if UI==Scissors && Ci==paper: User Wins;
-	if Ui==paper && Ci==paper: tie;
-	if UI==paper && Ci==scissors: Computer iwns;
-	if UI==paper && Ci==Rock: User wins;
+if UI==rock && CI==Scissors: User wins;
+if UI==Scissors && Ci==paper: User Wins;
+if UI==paper && Ci==Rock: User wins;
+if UI==rock && CI==paper: Computer Wins;
+if UI==paper && Ci==scissors: Computer iwns;
+if UI==Scissors && Ci==rock: Computer wins;
+if UI==rock && CI==rock: tie;
+if UI==Scissors && CI==Scissors: tie;
+if Ui==paper && Ci==paper: tie;
 -Score section
 -loop: ask player if play again? Y/N
 
@@ -22,11 +23,8 @@ TItle: shows title
 
 Display area: shows user input, computer input and score
 
-Selection section: Displays options for the users to click on, once clicked 
- it will highlight the chosen card and a button to proceed with the choice.
+Selection section: Displays options for the users to click on, once clicked
+it will highlight the chosen card and a button to proceed with the choice.
 
-
-
-pop up dialog box: pops up once the game is done (5 round) and asks user if 
- they want to play again.
-
+pop up dialog box: pops up once the game is done (5 round) and asks user if
+they want to play again.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let userScore = 0;
-let pcScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 let systemMsg = "";
+let scoreMsg = "";
 let userChoice = "";
 // document.getElementById("rock").addEventListener("click", () => {
 //   userChoice = "rock";
@@ -16,8 +17,8 @@ let userChoice = "";
 //   });
 
 function getComputerChoice() {
-  const chosen = Math.floor(Math.random() * 4);
   let chosenValue = "";
+  const chosen = Math.floor(Math.random() * 4);
   if (chosen == 1) {
     chosenValue = "rock";
   } else if (chosen == 2) {
@@ -29,10 +30,73 @@ function getComputerChoice() {
 }
 function rockChosen() {
   userChoice = "rock";
+  document.getElementById(
+    "user-display"
+  ).innerHTML = `User Selects: ${userChoice}`;
+  document.getElementById("status").innerHTML = "Click 'play' to continue";
 }
 function paperChosen() {
   userChoice = "paper";
+  document.getElementById(
+    "user-display"
+  ).innerHTML = `User Selects: ${userChoice}`;
+  document.getElementById("status").innerHTML = "Click 'play' to continue";
 }
 function scissorsChosen() {
   userChoice = "scissors";
+  document.getElementById(
+    "user-display"
+  ).innerHTML = `User Selects: ${userChoice}`;
+  document.getElementById("status").innerHTML = "Click 'play' to continue";
+}
+
+function play() {
+  if (userChoice.length == 0) {
+    document.getElementById("status").innerHTML = "Please select your choice";
+  } else {
+    getComputerChoice();
+    if (
+      (userChoice == "rock" && getComputerChoice() == "scissors") ||
+      (userChoice == "scissors" && getComputerChoice() == "paper") ||
+      (userChoice == "paper" && getComputerChoice() == "rock")
+    ) {
+      systemMsg = "Player Wins!";
+      playerScore++;
+      scoreMsg = `Score: Player:${playerScore}   Computer:${computerScore}`;
+    } else if (
+      (userChoice == "rock" && getComputerChoice == "paper") ||
+      (userChoice == "paper" && getComputerChoice() == "scissors") ||
+      (userChoice == "scissors" && getComputerChoice() == "rock")
+    ) {
+      computerScore++;
+      systemMsg = "Computer Wins!";
+      scoreMsg = `Score: Player:${playerScore}   Computer:${computerScore}`;
+    } else {
+      systemMsg = "its a tie!";
+      scoreMsg = `Score: Player:${playerScore}   Computer:${computerScore}`;
+    }
+    document.getElementById("status").innerHTML = systemMsg;
+    document.getElementById("score").innerHTML = scoreMsg;
+
+    if (playerScore == 5 || computerScore == 5) {
+      let winner = "";
+      if (playerScore == 5) {
+        winner = "Player";
+      } else {
+        winner = "Computer";
+      }
+      confirm(`${winner} Wins! 
+      Play again?`);
+      if (confirm == true) {
+        playerScore = 0;
+        computerScore = 0;
+        systemMsg = "Lets go again !";
+        document.getElementById("status").innerHTML = systemMsg;
+        document.getElementById("score").innerHTML = scoreMsg;
+      } else {
+        document.getElementById("main-container").style.display = "none";
+        document.getElementById("end-game").innerHTML = "GAME ENDED";
+      }
+    }
+  }
 }
